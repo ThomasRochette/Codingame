50309c6ab52209dc283d081f98b6f0702ba35f4c
Thomas <thomas.rochette0@gmail.com>
2020-04-11 21:34:35 +0200

    Entrainement v1

---
+++ ChuckNorris.py
@@ -0,0 +1,12 @@
+m=''.join(bin(ord(x))[2:].zfill(7) for x in input())
+b=''
+a=''
+for c in m:
+    if c == "1" != b:
+        a+=" 0 "
+        b="1"
+    elif c == "0" != b:
+        a+=" 00 "
+        b="0"
+    a+= "0"
+print(a.strip())
