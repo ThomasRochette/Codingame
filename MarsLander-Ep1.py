f0ccc1bdfc2aff6cceab8c829ead3c8f39bcf1ef
Thomas <thomas.rochette0@gmail.com>
2020-04-11 17:07:23 +0200

    Entrainement V2

--- MarsLander-Ep1.py
+++ MarsLander-Ep1.py
@@ -1,6 +1,4 @@
-n=input()
-for i in range(int(n)):
-    l=input()
 while True:
-    x,y,hs,vs,f,r,p = map(int,input().split())
-    print(vs<-39 and "0 4" or vs<10 and "0 3" or "0 0")
+    i = input().split()
+    if len(i)>2:
+        print(0,4*(int(i[3])<-39))
