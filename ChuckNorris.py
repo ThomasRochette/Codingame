093d84013e0e0a3591d2aab84682adafca6a6207
Thomas <thomas.rochette0@gmail.com>
2020-04-11 21:47:33 +0200

    V3 186 char

--- ChuckNorris.py
+++ ChuckNorris.py
@@ -1,11 +1,6 @@
 m=''.join(bin(ord(x))[2:].zfill(7) for x in input())
 a,b='',''
 for c in m:
-    if c=="1"!=b:
-        a+=" 0 "
-        b="1"
-    elif c=="0"!=b:
-        a+=" 00 "
-        b="0"
+    (a,b)=(c=="1"!=b) and (a+" 0 ","1") or (c=="0"!=b) and (a+" 00 ","0") or (a,b)
     a+= "0"
 print(a.strip())
