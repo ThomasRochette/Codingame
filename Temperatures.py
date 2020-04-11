a2b85bad42af426857030533014fe89ba751c0d5
Thomas <thomas.rochette0@gmail.com>
2020-04-11 14:33:57 +0200

    v2

--- Temperatures.py
+++ Temperatures.py
@@ -1,4 +1,3 @@
-n = int(input())
-t=list(map(int,input().split()))
-m=min(list(map(abs,t))) if len(t)!=0 else 0
-print(m if m in t else "-"+str(m) if m!=0 else "0")
+input()
+T=list(map(int,input().split()))
+print(T and sorted(sorted(T,reverse=True),key=abs)[0] or 0)
